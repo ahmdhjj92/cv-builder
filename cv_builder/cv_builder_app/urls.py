4093ea88699918f6392ef99ed1f90d6117cfd474
ahmdhjj92 <77078034+ahmdhjj92@users.noreply.github.com>
2021-09-08 21:51:00 +0300

    add link to 'about' page

--- cv_builder/cv_builder_app/urls.py
+++ cv_builder/cv_builder_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "cv_builder_app"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('about', views.about, name="about")
 ]
--- cv_builder/cv_builder_app/views.py
+++ cv_builder/cv_builder_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'cv_builder_app/index.html') 
+
+def about(request):
+    return render(request, 'cv_builder_app/about.html') 
