a60a027f2b8ccc7aada90ce3475ba5469868e9d2
ahmdhjj92 <77078034+ahmdhjj92@users.noreply.github.com>
2021-09-11 11:07:56 +0300

    add user registration form

--- cv_builder/users/views.py
+++ cv_builder/users/views.py
@@ -1,4 +1,18 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.urls import reverse
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
