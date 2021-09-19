d56026d1cadf3a8fbe06ffc425c24f185bfcd40e
ahmdhjj92 <77078034+ahmdhjj92@users.noreply.github.com>
2021-09-19 15:51:38 +0300

    improve registration form validation
    email is set to be unique and errors now show which fields were submitted empty

--- cv_builder/users/forms.py
+++ cv_builder/users/forms.py
@@ -1,10 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,error_messages={'required':'Email is required'})
+    first_name = forms.CharField(required=True, error_messages={'required':'First Name is required'})
+    last_name = forms.CharField(required=True, error_messages={'required':'Last Name is required'})
+    username = forms.CharField(required=True, error_messages={'required':'Username is required'})
+    password1 = forms.CharField(required=True, error_messages={'required':'Password is required'})
+    password2 = forms.CharField(required=True, error_messages={'required':'Password Confirmation is required'})
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with that email address already exists")
+        return email
